c8aa5ad8df949e50257b7415188fc170a808673a
Kyle Berry <kyle@kyleberry.com>
2018-06-15 16:31:06 -0600

    Iteration 1 'hamming.go'

---
+++ go/hamming/hamming.go
@@ -0,0 +1,22 @@
+// Package hamming calculates the Hamming Distance between two DNA strands
+package hamming
+
+import "errors"
+
+// Distance calculates the Hamming Distance between two DNA strands and returns
+// an error if the length of the strands is not equal.
+func Distance(a, b string) (int, error) {
+	var hammingDistance int
+
+	if len(a) != len(b) {
+		return 0, errors.New("DNA strands must be of equal length.")
+	}
+
+	for i := range a {
+		if a[i] != b[i] {
+			hammingDistance++
+		}
+	}
+
+	return hammingDistance, nil
+}
